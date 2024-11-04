60b60bef0a16f30b2a192205757604775f18c9c1
bharathsbankar <bharathbanakar954@gmail.com>
2024-11-05 00:05:58 +0530

    created graphs file

---
+++ DataStructures/graphs/graph.py
@@ -0,0 +1,31 @@
+#directed graph
+class Graph:
+
+    def __init__(self,edges):
+        self.edges=edges
+        self.graph_dict={}
+        for start , end in self.edges:
+            if start in self.graph_dict:
+                self.graph_dict[start].append(end)
+            else:
+                self.graph_dict[start]=[end]
+        print("graph_dictionary would be :",self.graph_dict)
+
+    def no_of_incom_nodes(self,node):
+
+        count=0
+        for i in self.edges:
+            if i[1]==node:
+                count+=1
+        return count
+
+
+if __name__=="__main__":
+    routes=[("RNR","DVG"),
+            ("DVG","BLR"),
+            ("CDR","BLR"),
+            ("RNR","CDR"),
+            ("CDR","DVG")
+            ]
+    g1=Graph(routes)
+
